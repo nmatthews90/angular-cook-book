1fec61bad0869042853e2a7835797682bcf008ff
Natasha Matthews <natashamatthews90@gmail.com>
2021-11-28 20:15:17 -0600

    added the shopping list and the recipe details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
-export class HearderComponent {}
+export class HeaderComponent {
+
+}
